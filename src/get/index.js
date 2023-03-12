41293adbefe1058452b67771ba862ce2e9e43b8e
abbassardar <abbassardar@hotmail.com>
2023-03-12 14:12:44 +0100

    Add calls

--- src/get/index.js
+++ src/get/index.js
@@ -1,15 +1,26 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  PutCommand,
+} from "@aws-sdk/lib-dynamodb";
+import { v4 as uuidv4, v6 as uuidv6 } from 'uuid';
+const client = new DynamoDBClient({});
 
-module.exports.post = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "Go Serverless v3.0! Your function executed successfully!",
-          input: event,
-        },
-        null,
-        2
-      ),
-    };
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const tableName = "skyworkz-news";
+
+
+
+module.exports.post = async (event, context) => {  
+  generatedId = uuidv4;
+  body = await dynamo.send(
+    new ScanCommand({ TableName: tableName })
+  );
+  body = body.Items;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(body),
   };
-  
+};
--- src/post/index.js
+++ src/post/index.js
@@ -1,10 +1,37 @@
-module.exports.post = async (event) => {
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  PutCommand,
+} from "@aws-sdk/lib-dynamodb";
+import { v4 as uuidv4, v6 as uuidv6 } from 'uuid';
+const client = new DynamoDBClient({});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const tableName = "skyworkz-news";
+
+
+
+module.exports.post = async (event, context) => {  
+  generatedId = uuidv4;
+  let requestJSON = JSON.parse(event.body);
+        await dynamo.send(
+          new PutCommand({
+            TableName: tableName,
+            Item: {
+              id: generatedId,
+              title: requestJSON.title,
+              date: requestJSON.date,
+              description: requestJSON.description,
+            },
+          })
+        );
+        body = `Put item ${generatedId}`;
   return {
-    statusCode: 200,
+    statusCode: 201,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+        message: "Item created with id: ", generatedId,
       },
       null,
       2
