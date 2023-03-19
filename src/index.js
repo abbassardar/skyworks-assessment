2f1963caa1f3fed8f7fc0c707928f51b12828671
abbassardar <abbassardar@hotmail.com>
2023-03-19 15:33:28 +0100

    Add logic

--- src/index.js
+++ src/index.js
@@ -1,34 +1,74 @@
 var AWS = require('aws-sdk');
+AWS.config.update({
+  region:'eu-west-1'
+});
 
-const client = new DynamoDBClient({});
-const dynamo = DynamoDBDocumentClient.from(client);
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const tableName = "skyworkz-news";
 
-module.exports.post = async (event, context) => {  
-  generatedId = uuid.v4;
-  let requestJSON = JSON.parse(event.body);
-        await dynamo.send(
-          new PutCommand({
-            TableName: tableName,
-            Item: {
-              id: generatedId,
-              title: requestJSON.title,
-              date: requestJSON.date,
-              description: requestJSON.description,
-            },
-          })
-        );
-        body = `Put item ${generatedId}`;
-
-  event.body
-  return {
-    statusCode: 201,
-    body: JSON.stringify(
-      {
-        message: "Item created with id: ", generatedId,
-      },
-      null,
-      2
-    ),
-  };
+module.exports.handler = async (event) => {  
+ let response;
+ switch(true){
+  case event.httpMethod === 'GET' :
+    response =  await getItems();
+    break;
+  case event.httpMethod === 'POST' :
+    response = await saveItem(JSON.parse(event.body));
+    break;
+ }
+ return response;
 };
+
+
+async function getItems(){
+  const params ={
+    TableName: tableName
+  }
+  const allItems = await scanDynamoRecords(params, []);
+  const body = {
+    items: allItems
+  }
+  return buildResponse(200, body)
+}
+
+async function scanDynamoRecords(scanParams, itemsArray){
+  try{
+  const dynamoData = await dynamoDB.scan(scanParams).promise();
+  itemsArray = itemsArray.concat(dynamoData.Items);
+    if(dynamoData.LastEvaluatedKey){
+      scanParams.ExclusiveStartKey = dynamoData.LastEvaluatedKey
+      return await scanDynamoRecords(scanParams, itemsArray)
+    }
+  return itemsArray
+}catch(error){
+  console.error("");
+}
+}
+
+async function saveItem(requestBody){
+  const params = {
+    TableName: tableName,
+    Item: requestBody
+  }
+  return await dynamoDB.put(params).promise().then(() => {
+    const body = {
+      Operation: 'SAVE',
+      Message: 'SUCCESS',
+      Item: requestBody
+    }
+    return buildResponse(200, body);
+  }, (error) => {
+    console.error("");
+  })
+}
+
+
+function buildResponse(statusCode, body){
+return {
+  StatusCode: statusCode,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(body)
+  }
+}
