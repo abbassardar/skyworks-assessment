591cc9db5f9e9f861cd94dfcdd103877b2ade309
abbassardar <abbassardar@hotmail.com>
2023-03-19 17:25:56 +0100

    add logging to debug

--- src/index.js
+++ src/index.js
@@ -1,51 +1,65 @@
 var AWS = require('aws-sdk');
 AWS.config.update({
-  region:'eu-west-1'
+  region: 'eu-west-1'
 });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const tableName = 'skyworkz-news';
 
-module.exports.handler = async (event) => {  
- let response;
- switch(true){
-  case event.httpMethod === 'GET' :
-    response =  await getItems();
-    break;
-  case event.httpMethod === 'POST' :
-    response = await saveItem(JSON.parse(event.body));
-    break;
- }
- return response;
+module.exports.handler = async (event) => {
+  console.log("test");
+  let response;
+  switch (true) {
+    case event.httpMethod === 'GET':
+      console.log("test get");
+
+      response = await getItems();
+      console.log("test after get");
+
+      break;
+    case event.httpMethod === 'POST':
+      console.log("test post ");
+
+      response = await saveItem(JSON.parse(event.body));
+      console.log("testafter post");
+
+      break;
+  }
+  return response;
 };
 
 
-async function getItems(){
-  const params ={
+async function getItems() {
+  const params = {
     TableName: tableName
   }
+  console.log("test before scan");
+
   const allItems = await scanDynamoRecords(params, []);
   const body = {
     items: allItems
   }
+  console.log("test after scan");
+  console.log("testitems ", allItems);
+
   return buildResponse(200, body)
 }
 
-async function scanDynamoRecords(scanParams, itemsArray){
-  try{
-  const dynamoData = await dynamoDB.scan(scanParams).promise();
-  itemsArray = itemsArray.concat(dynamoData.Items);
-    if(dynamoData.LastEvaluatedKey){
+async function scanDynamoRecords(scanParams, itemsArray) {
+  try {
+    const dynamoData = await dynamoDB.scan(scanParams).promise();
+    itemsArray = itemsArray.concat(dynamoData.Items);
+    if (dynamoData.LastEvaluatedKey) {
       scanParams.ExclusiveStartKey = dynamoData.LastEvaluatedKey
       return await scanDynamoRecords(scanParams, itemsArray)
     }
-  return itemsArray
-}catch(error){
-  console.error("problem scanning items", error);
-}
+    return itemsArray
+  } catch (error) {
+    console.error("problem scanning items", error);
+  }
 }
 
-async function saveItem(requestBody){
+async function saveItem(requestBody) {
   const params = {
     TableName: tableName,
     Item: requestBody
@@ -63,12 +77,12 @@ async function saveItem(requestBody){
 }
 
 
-function buildResponse(statusCode, body){
-return {
-  StatusCode: statusCode,
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(body)
+function buildResponse(statusCode, body) {
+  return {
+    StatusCode: statusCode,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
   }
 }
